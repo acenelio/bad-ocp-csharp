6390767e007471d2420f12e5d31c50f010e42e9c
Nelio Alves <acenelio@gmail.com>
2018-05-14 15:02:40 -0300

    Initial implementation

---
+++ Course/Services/TaxService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Course.Services {
+    class TaxService {
+
+        public double Tax(double amount, string country) {
+
+            if (country == "BRA") {
+                if (amount <= 100.00) {
+                    return amount * 0.2;
+                }
+                else {
+                    return amount * 0.15;
+                }
+            }
+            else if (country == "USA") {
+                return amount * 0.12;
+            }
+            else {
+                throw new ArgumentException("Invalid country");
+            }
+        }
+    }
+}
